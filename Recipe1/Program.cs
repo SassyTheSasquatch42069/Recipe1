54000b754e19dd93e2b775b9b671b080b0fe9047
Caleb <st10157171@rcconnect.edu.za>
2023-04-27 16:19:35 +0200

    making the array

--- Recipe1/Program.cs
+++ Recipe1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Recipe
+{
+    // Making the array
+    private string[] ingredients;
+    private double[] quantity;
+    private string[] measurement;
+    private string[] steps;
+    
+}
