d461fcc172b90c4bd4c9a7a2d73cb1ebad052f52
Caleb <st10157171@rcconnect.edu.za>
2023-04-27 16:22:22 +0200

    making the arrays empty

--- Recipe1/Program.cs
+++ Recipe1/Program.cs
@@ -5,5 +5,13 @@
     private double[] quantity;
     private string[] measurement;
     private string[] steps;
-    
+
+      public Recipe()
+      {
+        // Making the arrays empty so that there is space to add info in ingredients, quanitity, measurement and steps.
+        ingredients = new string[0];
+        quantity = new double[0];
+        measurement = new string[0];
+        steps = new string[0];
+      }
 }
