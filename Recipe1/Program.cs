16e77db3cd54b913add103adf0ebcd917a623891
Caleb <st10157171@rcconnect.edu.za>
2023-04-27 17:27:33 +0200

    Printing the ingredients and measurements

--- Recipe1/Program.cs
+++ Recipe1/Program.cs
@@ -6,12 +6,24 @@
     private string[] measurement;
     private string[] steps;
 
-      public Recipe()
-      {
+    public Recipe()
+    {
         // Making the arrays empty so that there is space to add info in ingredients, quanitity, measurement and steps.
         ingredients = new string[0];
         quantity = new double[0];
         measurement = new string[0];
         steps = new string[0];
-      }
+    }
+    public void ShowRecipe()
+    {
+        // Display the ingredients and measurements
+        Console.WriteLine("Ingredients:");
+        for (int w = 0; w < ingredients.Length; w++)
+        {
+            Console.WriteLine($"- {quantity[w]} {measurement[w]} of {ingredients[w]}");
+        }
+    
+        
+    }
+
 }
